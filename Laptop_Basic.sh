6dbaf933fea5d1fdcd6e0ca902090ce6e25e84e9
JeonghunLee <ahyuo79@gamil.com>
2019-09-07 23:31:12 +0900

    Updated Laptop Check Drivers

--- Laptop_Basic.sh
+++ Laptop_Basic.sh
@@ -5,10 +5,13 @@
 #
 
 LAPTOP_NAME=Lenovo_Y540
+DATE=$(date)
 
-echo "Start Checking $LATOP_NAME  Laptop"
+echo $DATE
+echo "Start Checking $LAPTOP_NAME Laptop"
 
 checkLaptop() {
+
 
    ARCHITECTURE=`uname -m`
    if [ "$ARCHITECTURE" != "x86_64" ] && [ "$ARCHITECTURE" != "aarch64" ] ; then
@@ -16,8 +19,11 @@ checkLaptop() {
        exit 1
    fi
 
-   CHECK_IDEAPAD=`lsmod | grep ideapad_laptop`
+   echo "Checked your CPU $ARCHITECTURE "
 
+
+
+   CHECK_IDEAPAD=`lsmod | grep ideapad_laptop`
    if [ $CHECK_IDEAPAD ]; then
 
        echo "found wrong $CHECK_IDEAPAD module "
@@ -30,22 +36,32 @@ checkLaptop() {
           echo "now already added ideapad_laptop in blacklist.conf"
        else	        
 	  if [ -f $BLACKLIST ]; then
-              UPDATE=`sudo echo "#for only $LAPTOP_NAME wifi " >> $BLACKLIST`
+              UPDATE=`sudo echo "#for only $LAPTOP_NAME wifi"  >> $BLACKLIST`
               UPDATE=`sudo echo "blacklist ideapad_laptop"     >> $BLACKLIST`
               TEST0=`tail /etc/modprobe.d/blacklist.conf`
 	      echo "$TEST0"
+	      echo "Updated Blacklist file ok"
           fi 
        fi
+   else
+       echo "WIFI Driver Ok "
    fi	   
+   echo "Checked your WIFI Driver "
 
-   DISKSIZE=$(df -h)
-   echo "$DISKSIZE"
+
 }
 
 checkLinux() {
-   DATE=$(date)
-   echo $DATE
 
+   UBUNTU=`uname -v | grep Ubuntu`
+
+   if [ ${UBUNTU:0:8} ]; then
+       VERSION=`lsb_release -sr`
+       echo "Check Linux: Ubuntu $VERSION"
+   fi 	   
+
+   #release="ubuntu"$(lsb_release -sr | sed -e "s/\.//g")
+   #echo $UBUNTU
 }
 
 checkLaptop
